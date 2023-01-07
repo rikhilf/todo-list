860921d995fc221b99ad77883572c901d760377e
Rikhil Fellner <rfellner@unc.edu>
2023-01-06 20:11:22 -0500

    bonus progress

--- app.py
+++ app.py
@@ -18,11 +18,12 @@ def create_todo():
 
         title: str = request.form['title']
         description: str = request.form['description']
+        color: str = request.form['color']
 
         if title == '':
             return render_template("create-todo.html")
 
-        new_todo: todo = todo(todo_count, title, description)
+        new_todo: todo = todo(todo_count, title, description, color)
         todo_list.append(new_todo)
 
         todo_count += 1
@@ -31,9 +32,32 @@ def create_todo():
     return render_template("create-todo.html")
 
 
-@app.route('/view-todo-list')
+@app.route('/view-todo-list', methods=["GET", "POST"])
 def view_todo_list():
+    if request.method == "POST" and len(todo_list) > 0:
+        idx: int = int(request.form['check-button'])
+        todo_list[idx].checked = not todo_list[idx].checked
     return render_template('view-list.html', todo_list=todo_list)
+
+
+@app.route('/edit-todo<todo_number>', methods=["GET", "POST"])
+def edit_todo(todo_number: str):
+    if request.method == "POST":
+        global todo_list
+
+        title: str = request.form['title']
+        description: str = request.form['description']
+        color: str = request.form['color']
+
+        if title == '':
+            return render_template("edit-todo.html")
+
+        todo_list[int(todo_number)].title = title
+        todo_list[int(todo_number)].description = description
+        todo_list[int(todo_number)].color = color
+
+        return render_template("edit-success.html")
+    return render_template('edit-todo.html', todo=todo_list[int(todo_number)])
 
 if __name__ == '__main__':
     app.run(debug=True)
--- helpers.py
+++ helpers.py
@@ -2,8 +2,12 @@ class todo:
     id: int
     title: str
     description: str
+    color: str
+    checked: bool
 
-    def __init__(self, id: int, title: str, description: str):
+    def __init__(self, id: int, title: str, description: str, color: str):
         self.id = id
         self.title = title
         self.description = description
+        self.color = color
+        self.checked = False
